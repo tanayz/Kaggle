61871220dfcea50a335141b51abb5981de7ecd38
Tanay Chowdhury <tanayz@outlook.com>
2015-06-30 19:13:48 -0500

    Final file-though couldn't submit

--- Bag_of_words/linear.py
+++ Bag_of_words/linear.py
@@ -85,31 +85,6 @@ X_test = X_all[lentrain:]
 ## Get best model
 #best_model = model.best_estimator_
 
-
-model1 = LogisticRegression(penalty='l2', dual=True, tol=0.0001, 
-                         C=1, fit_intercept=True, intercept_scaling=1.0, 
-                         class_weight=None, random_state=None)
-model2 = SVC(C=1.0, kernel='rbf', degree=3, gamma=0.001, coef0=0.0, shrinking=True, 
-            probability=False, tol=0.0001, cache_size=200, class_weight=None, verbose=False, 
-            max_iter=-1, random_state=None)
-model3 = SVC(C=1.0, kernel='linear', degree=3, gamma=0.001, coef0=0.0, shrinking=True, 
-            probability=False, tol=0.0001, cache_size=200, class_weight=None, verbose=False, 
-            max_iter=-1, random_state=None)
-
-
-print "5 Fold CV Score from model1: ", np.mean(cross_validation.cross_val_score(model1, X, y, cv=5, scoring='roc_auc'))
-print "5 Fold CV Score from model2: ", np.mean(cross_validation.cross_val_score(model2, X, y, cv=5, scoring='roc_auc'))
-print "5 Fold CV Score from model3: ", np.mean(cross_validation.cross_val_score(model3, X, y, cv=5, scoring='roc_auc'))
-
-
-print "Retrain on all training data, predicting test labels...\n"
-model1.fit(X,y)
-model2.fit(X,y)
-model3.fit(X,y)
-result1 = model1.predict_proba(X_test)[:,1]
-result2 = model2.predict_proba(X_test)[:,1]
-result3 = model3.predict_proba(X_test)[:,1]
-testdata.append(" ".join(KaggleWord2VecUtility.review_to_wordlist(test["review"][i], False)))
 print 'vectorizing... ', 
 tfv = TfidfVectorizer(min_df=3,  max_features=None, 
         strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
