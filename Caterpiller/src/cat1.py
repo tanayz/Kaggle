3edd1e9bd6f41263faf6a1210c21d04d37534b86
Tanay Chowdhury <tanayz@outlook.com>
2015-07-08 06:58:19 -0500

    Combined featues,handled nan,yet to handle categorical values

--- Caterpiller/src/cat1.py
+++ Caterpiller/src/cat1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import glob
 import os
+from sklearn.ensemble import RandomForestRegressor
 
 ### Load train and test
 test = pd.read_csv("../input/test_set.csv")
@@ -24,28 +25,61 @@ while(continueLoop):
             continueLoop=True
             print train.shape
         
+from sklearn.base import TransformerMixin
 
-#### Clean NA values
-#for(i in 1:ncol(train)){
-#  if(is.numeric(train[,i])){
-#    train[is.na(train[,i]),i] = -1
-#  }else{
-#    train[,i] = as.character(train[,i])
-#    train[is.na(train[,i]),i] = "NAvalue"
-#    train[,i] = as.factor(train[,i])
-#  }
-#}
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self):
+        """Impute missing values.
+
+        Columns of dtype object are imputed with the most frequent value 
+        in column.
+
+        Columns of other types are imputed with mean of column.
+
+        """
+    def fit(self, X, y=None):
+
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+
+train = DataFrameImputer().fit_transform(train)
+
+test = train[train['id']>0]
+train = train[train['id']<0]
+
+rf = RandomForestRegressor(random_state=0, n_estimators=100)
+y=np.log(train['cost']+1)
+X= train[train.columns.difference(train.columns[[2, 3]])]
+rf.fit(X,y)
+
+
+
+
+#### Train randomForest on the whole training set
+#rf = randomForest(log(train$cost + 1)~., train[,-match(c("id", "cost"), names(train))], ntree = 30, do.trace = 2)
 #
-#### Clean variables with too many categories
-#for(i in 1:ncol(train)){
-#  if(!is.numeric(train[,i])){
-#    freq = data.frame(table(train[,i]))
-#    freq = freq[order(freq$Freq, decreasing = TRUE),]
-#    train[,i] = as.character(match(train[,i], freq$Var1[1:30]))
-#    train[is.na(train[,i]),i] = "rareValue"
-#    train[,i] = as.factor(train[,i])
-#  }
-#}
+#pred = exp(predict(rf, test)) - 1
 #
-#test = train[which(train$id > 0),]
-#train = train[which(train$id < 0),]
+#submitDb = data.frame(id = test$id, cost = pred)
+#submitDb = aggregate(data.frame(cost = submitDb$cost), by = list(id = submitDb$id), mean)
+#
+#write.csv(submitDb, "submit.csv", row.names = FALSE, quote = FALSE)
+
+
+##### Clean NA values
+#
+#for i in range(len(train.columns)):
+#    if train.ix[:,i].dtypes in ['int','float']:
+#      train.ix[:,i].fillna(-1) 
+#    else:
+#       train.ix[:,i].fillna("NAvalue") 
+#
+##### Clean variables with too many categories
+
